76c48779a9e5ebc0a71e32b7b394c85365025a9c
tomasgerardi7 <tgerardi@est.frba.utn.edu.ar>
2020-05-11 16:11:51 -0300

    delted

--- 21-04_ejercitacion.hs
+++
@@ -1,8 +0,0 @@
--- 1. Buscar el maximo número primo
--- NOTA: Un numero es primo si y solamente si es divisible por 1 y por si mismo
-
-buscarPrimoMax :: Int->[Int]->[Int]
-buscarPrimoMax nro lista | (mod nro  1 == 0) && (mod nro nro == 0) = nro:lista
-                         | otherwise = buscarPrimoMax (nro + 1) lista
-
-
